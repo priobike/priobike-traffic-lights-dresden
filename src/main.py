d4ce6244727da14fd9b7f1ab6ed555ddbe6a9813
Philipp Matthes <philipp.matthes@tu-dresden.de>
2024-03-14 14:31:08 +0100

    🤣

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import asyncio
 
 from converter import run_tls_message_converter
 from runner import run_message_generator
-from syncer import get_all_things
+from syncer import sync_things
 
-things = get_all_things()
+things = sync_things()
 print(f'{len(things)} Things in FROST server.')
 things_for_message_generator = [t for t in things if t['name'] != 'SG1' and t['name'] != 'SG2']
 things_for_tls_message_converter = [t for t in things if t['name'] == 'SG1' or t['name'] == 'SG2']
